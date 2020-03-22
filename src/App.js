b46cdca03211639ca53f585c33dea55697e41501
Johnny Araújo <ijohnnysa@gmail.com>
2020-03-22 17:29:34 -0300

    Rocketfy React app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { DndProvider } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
+
+import Header from "./components/Header";
+import Board from "./components/Board";
+
+import GlobalStyle from "./styles/global";
+
+function App() {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <GlobalStyle />
+      <Header />
+      <Board />
+    </DndProvider>
+  );
+}
+
+export default App;
---
+++ src/components/List/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import Card from "../Card";
+
+import { MdAdd } from "react-icons/md";
+
+import Container from "./styles";
+
+function List({ data, index: listIndex }) {
+  return (
+    <Container done={data.done}>
+      <header>
+        <h2>{data.title}</h2>
+        {data.creatable && (
+          <button type="button">
+            <MdAdd size={24} color="#ffffff" />
+          </button>
+        )}
+      </header>
+
+      <ul>
+        {data.cards.map((card, index) => (
+          <Card key={card.id} index={index} listIndex={listIndex} data={card} />
+        ))}
+      </ul>
+    </Container>
+  );
+}
+
+export default List;
